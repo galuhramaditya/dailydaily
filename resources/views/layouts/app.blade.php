50372cf43929683571853c77e5623fb6fd2c6629
maulanairfanf <maulanairfanf@gmail.com>
2021-06-06 15:15:18 +0700

    landing page succes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@0;900&display=swap" rel="stylesheet">
     <title>Dailydaily</title>
@@ -22,6 +23,60 @@
             font-weight: bold;
             border-bottom: 3px #00C46F solid;
         }
+
+        .title-hero {
+            z-index: 99;
+        }
+
+        .fs-30 {
+            font-size: 30px;
+        }
+
+        .fs-15 {
+            font-size: 15px;
+        }
+
+        .fs-45 {
+            font-size: 45px;
+        }
+
+        .continue-button{
+            transform : translate(-0,50%);
+        }
+
+        .order-services{
+            transform : translate(0,-50%)
+        }
+
+        .hero {
+            background-image: url("img/hero.jpg");
+            height: 1013px;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+         .vertical-line{
+        display: inline-block;
+        border-left: 3px solid #ccc;
+   
+        height: 125px;
+    }
+
+        .sub-hero {
+            margin: 0 0 15em 5em;
+        }
+
+        .sub-hero span {
+            color: #00C46F
+        }
+
+        .icon-order .card {
+            width: 118px;
+            height: 118px;
+            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        }
+
+
     </style>
 </head>
 
@@ -30,10 +85,13 @@
 
     @yield("content")
     @include("layouts.components.hero")
-
+    @include("layouts.components.order-services")
+    @include("layouts.components.our-services")
     @include("layouts.components.footer")
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,31 +1,31 @@
-<footer>
-    <div class="container">
+<footer style="background-color : #00C46F" >
+    <div class="container py-5 text-white">
         <div class="row">
             <div class="col">
-                <div class="h4">ACCOUNT</div>
-                <div>Sign in</div>
-                <div>register</div>
-                <div>Order Status</div>
+                <div class="fs-30">ACCOUNT</div>
+                <div class="fs-15">Sign in</div>
+                <div class="fs-15">register</div>
+                <div class="fs-15">Order Status</div>
             </div>
             <div class="col">
-                <div class="h4">ABOUT</div>
-                <div>Our Story</div>
-                <div>Media</div>
-                <div>Contact</div>
+                <div class="fs-30">ABOUT</div>
+                <div class="fs-15">Our Story</div>
+                <div class="fs-15">Media</div>
+                <div class="fs-15">Contact</div>
             </div>
             <div class="col">
-                <div class="h4">HELP</div>
-                <div>Guides</div>
-                <div>Complain</div>
-                <div>Maintenance</div>
+                <div class="fs-30">HELP</div>
+                <div class="fs-15">Guides</div>
+                <div class="fs-15">Complain</div>
+                <div class="fs-15">Maintenance</div>
             </div>
             <div class="col">
-                <div class="h4">LEGAL STUFF</div>
-                <div>Terms of use</div>
-                <div>Terms of services</div>
-                <div>Privacy policy</div>
+                <div class="fs-30">LEGAL STUFF</div>
+                <div class="fs-15">Terms of use</div>
+                <div class="fs-15">Terms of services</div>
+                <div class="fs-15">Privacy policy</div>
             </div>
         </div>
-        <div class="row"></div>
+        <div class="fs-15 mt-3" >dailydaily Company. All Rights Reserved</div>
     </div>
 </footer>
